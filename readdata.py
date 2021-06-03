f74cc2f3c8ef4e8d1e5a6af521f7f04aa18b2f99
Haofan Li <fiannahaofanli@gmail.com>
2021-06-03 15:10:05 -0600

    refactor scripts to use new module, updated readdata.py

--- readdata.py
+++ readdata.py
@@ -17,16 +17,16 @@ def read_data(columns, types={},filename="data/wxobs20170821.txt"):
     with open(filename, 'r') as datafile:
         # read the first 3 lines (header)
         for _ in range (3):
-        headerline = datafile.readline()
-        print(headerline)
+            datafile.readline()
+        # print(headerline)
     # read and parse the rest of the file
-    for line in datafile:
-        split_line = line.split()
-        for column in columns:
-            i = columns[column]
-            t = types.get(column, str)
-            value = t(split_line[i])
-            data[column].append(value)
+        for line in datafile:
+            split_line = line.split()
+            for column in columns:
+                i = columns[column]
+                t = types.get(column, str)
+                value = t(split_line[i])
+                data[column].append(value)
 
     return data
 
