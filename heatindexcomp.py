113f4ee30cdf92154eebdd0207c3e143aedc1b74
Haofan Li <fiannahaofanli@gmail.com>
2021-06-03 15:06:57 -0600

    calls the read_data function when calculating heat index

--- heatindexcomp.py
+++ heatindexcomp.py
@@ -1,3 +1,4 @@
+from readdata import read_data
 
 # Colomn names and column indices to read
 columns = {'date':0, 'time':1, 'tempout':2, 'humout':5, 'heatindex':13} 
@@ -10,26 +11,8 @@ data = {}
 for column in columns:
     data[column] = []
 
-# Initialize my data variable
-# data = {'date':[], 'time':[],'tempout':[], 'windspeed':[]}
-
-#Read the data file
-
-filename = "data/wxobs20170821.txt"
-
-with open(filename, 'r') as datafile:
-    # read the first 3 lines (header)
-    for _ in range (3):
-        headerline = datafile.readline()
-        print(headerline)
-    # read and parse the rest of the file
-    for line in datafile:
-        split_line = line.split()
-        for column in columns:
-            i = columns[column]
-            t = types.get(column, str)
-            value = t(split_line[i])
-            data[column].append(value)
+# Read the data file
+data = read_data(columns,types=types)
 
 # Compute the heat index 
 def compute_heatindex(t,hum):
