e0bda900f7c2cd9ecd0176431122b1994fec8ec1
Haofan Li <fiannahaofanli@gmail.com>
2021-06-03 12:27:12 -0600

    compute wind chill factor

--- mysci.py
+++ mysci.py
@@ -1,9 +1,9 @@
 
 # Colomn names and column indices to read
-columns = {'date':0,'time':1,'tempout':2,'windspeed':7}
+columns = {'date':0, 'time':1, 'tempout':2, 'windspeed':7}
 
-# Data types for each column (only is non-string)
-types = {'tempout':float,'windspeed':float}
+# Data types for each column (only if non-string)
+types = {'tempout': float,'windspeed': float}
 
 # Initialize my data variable
 data = {}
@@ -11,7 +11,7 @@ for column in columns:
     data[column] = []
 
 # Initialize my data variable
-data = {'date':[], 'time':[],'tempout':[]}
+data = {'date':[], 'time':[],'tempout':[], 'windspeed':[]}
 
 #Read the data file
 
@@ -21,7 +21,7 @@ with open(filename, 'r') as datafile:
     # read the first 3 lines (header)
     for _ in range (3):
         headerline = datafile.readline()
-        #print(headerline)
+        print(headerline)
     # read and parse the rest of the file
     for line in datafile:
         split_line = line.split()
@@ -31,8 +31,22 @@ with open(filename, 'r') as datafile:
             value = t(split_line[i])
             data[column].append(value)
 
+# Compute wind chill temperature
+def compute_windchill(t,v):
+    a = 35.74
+    b = 0.6215
+    c = 35.75
+    d = 0.4275
+    v16 = v**0.16
+    wci = a+(b*t)-(c*v16)+(d*t*v16)
+    return wci
 
+# Running the function to compute wci
+windchill = []
+for temp, windspeed in zip(data['tempout'], data['windspeed']):
+    windchill.append(compute_windchill(temp,windspeed))
 
+print (windchill)
 
 
 # DEBUG
@@ -42,7 +56,8 @@ with open(filename, 'r') as datafile:
 #print(data[8][0:-1:2])
 #print(data['tempout'])
 
-
+for i,j in zip([1,2],[3,4,5]):
+    print (i,j)
 
 
 
