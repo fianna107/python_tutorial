02da2468b50cbf55a01704c441ad38ff5015f52a
Haofan Li <fiannahaofanli@gmail.com>
2021-06-04 14:16:18 -0600

    adding setup.py

---
+++ setup.py
@@ -0,0 +1,13 @@
+
+from setuptools import setup
+
+setup(name="mysci", version="1.0.0", description="A sample package", author="FiannaLi", author_email="fiannahaofanli@gmail.com",packages=["mysci"], install_requires=[])
+
+
+
+
+
+
+
+
+
